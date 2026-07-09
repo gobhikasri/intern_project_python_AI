1fbb177a0f89705b8243fb83a69878039b43a55c
gobhikasri <166229538+gobhikasri@users.noreply.github.com>
2026-07-09 11:57:12 +0530

    Add files via upload

---
+++ NinoSense/app.py
@@ -0,0 +1,68 @@
+from flask import Flask, render_template
+import pandas as pd
+from statsmodels.tsa.arima.model import ARIMA
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+app = Flask(__name__)
+
+# Risk classification
+def classify_risk(oni):
+    if oni < 0.5:
+        return "LOW RISK"
+    elif oni <= 1.5:
+        return "MEDIUM RISK"
+    else:
+        return "HIGH RISK"
+
+@app.route("/")
+def dashboard():
+
+    # Load NOAA data
+    df = pd.read_csv("dataset/noaa_data.csv")
+
+    # Current ONI
+    current_oni = round(df["Value"].iloc[-1], 2)
+
+    # Forecast
+    model = ARIMA(df["Value"], order=(5, 1, 0))
+    model_fit = model.fit()
+
+    forecast = model_fit.forecast(steps=6)
+
+    forecast_data = []
+
+    high_risk_found = False
+
+    current_date = datetime.now()
+
+    for i, value in enumerate(forecast, start=1):
+        future_date = current_date + relativedelta(months=i)
+        risk = classify_risk(value)
+        if risk == "HIGH RISK":
+            high_risk_found = True
+        forecast_data.append({
+        "date": future_date.strftime("%b %Y"),
+        "oni": round(float(value), 2),
+        "risk": risk
+    })
+
+    overall_alert = (
+        "⚠️ EL NIÑO WARNING DETECTED"
+        if high_risk_found
+        else "✅ NO MAJOR EL NIÑO WARNING"
+    )
+
+    return render_template(
+        "dashboard.html",
+        current_oni=current_oni,
+        forecast_data=forecast_data,
+        overall_alert=overall_alert
+    )
+
+@app.route("/map")
+def climate_map():
+    return render_template("map.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
